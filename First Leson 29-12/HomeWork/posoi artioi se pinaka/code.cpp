f9fd62f8b64b23531854a85efc560858f08ab672
MiltosSaratzidis <m.saratzidis@gmail.com>
2016-02-12 20:25:41 +0200

    a

--- First Leson 29-12/HomeWork/posoi artioi se pinaka/code.cpp
+++ First Leson 29-12/HomeWork/posoi artioi se pinaka/code.cpp
@@ -5,28 +5,27 @@ using namespace std;
 int main()
 {
 
-	int table[5],i,u,x;
+	int table[5],i,u,x,m;
 
     for(i=0;i<5;i=i+1)
     {
         cin>>table[i];
     }
+
+    m=0;
+
     for(i=0;i<5;i=i+1)
     {
         u=table[i]%2;
 
         if(u==1)
         {
-            cout<<table[i]<<endl;
+         m=m+1;
         }
-
-
-
-
 
     }
 
-
+    cout<<m<<endl;
 
 	return 0;
 
